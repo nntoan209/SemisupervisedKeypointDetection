fix NME formula and evaluation meter

scratch/metrics/nme.py
@@ -14,17 +14,17 @@ class NME:
         if normalize_item == 'bbox_size':
             batch_size = items['bbox'].shape[0]
             bbox_shapes = torch.abs(items['bbox'][:, :, :2] - items['bbox'][:, :, 2:]).reshape(batch_size, 2)
-            return torch.prod(bbox_shapes, dim=1)
+            return torch.sqrt(torch.prod(bbox_shapes, dim=1))
         
         if normalize_item == 'image_size':
             image_sizes = torch.stack((items['img_shape'][0], items['img_shape'][1]), dim=1) 
-            return torch.prod(image_sizes, dim=1)
+            return torch.sqrt(torch.prod(image_sizes, dim=1))
         
         if normalize_item == 'input_size':
             # all items must have the same size
-            input_size_0 = items['input_size'][0][0].item()
-            input_size_1 = items['input_size'][1][0].item()
-            return input_size_0 * input_size_1 
+            input_size_0 = items['input_size'][0][0]
+            input_size_1 = items['input_size'][1][0]
+            return torch.sqrt(input_size_0 * input_size_1)
         
     def __call__(self, batch_keypoints: np.ndarray, items: dict):
         """
@@ -35,15 +35,15 @@ class NME:
                 D: dimension of each keypoints (2)
             items: contains the information of the items in the batch
         Returns:
-            results: array of shape [B, ]
+            results: the mean NME of the batch keypoints
         """
         batch_size, _, num_keypoints, _ = batch_keypoints.shape
         gt_batch_keypoints = items['keypoints'].detach().cpu().numpy()
         normalize_factor = self._get_normalize_factor(items=items,
-                                                normalize_item=self.normalize_item)
+                                                      normalize_item=self.normalize_item)
         displacement = batch_keypoints - gt_batch_keypoints
         distances = np.linalg.norm(displacement, axis=-1).reshape(batch_size, num_keypoints)
         results = np.sum(distances, axis=1) / normalize_factor
         
-        return torch.sum(results)
+        return torch.mean(results)
     

scratch/network/mean_teacher_network.py
@@ -34,11 +34,10 @@ class MeanTeacherNetwork(nn.Module):
             t_keypoints_pred, t_keypoints_pred_score = self.teacher_model.predict(items, cuda)
             
             # predicted keypoints of the student model
-            s_keypoints_pred, s_keyoints_pred_score = self.student_model.predict(items, cuda)
+            s_keypoints_pred, s_keypoints_pred_score = self.student_model.predict(items, cuda)
             
-        return t_keypoints_pred, t_keypoints_pred_score, s_keypoints_pred, s_keyoints_pred_score
-        
-    
+        return t_keypoints_pred, t_keypoints_pred_score, s_keypoints_pred, s_keypoints_pred_score
+
     def _update_teacher_ema(self, ema_decay):
         for t_param, s_param in zip(self.teacher_model.parameters(), self.student_model.parameters()):
             t_param.data.mul_(ema_decay).add_(other=s_param.data, alpha=1 - ema_decay)

scratch/trainer/trainer.py
@@ -93,6 +93,8 @@ class EMATrainer:
             batch_image = batch['img'].to(self.device)
             batch_heatmap = batch['heatmap'].to(self.device)
             
+            num_item = batch_image.shape[0]
+            
             with torch.no_grad():
                 t_heatmap_pred, s_heatmap_pred = self.network(batch_image)
                 t_keypoints_pred, _, s_keypoints_pred, _ = self.network.module.predict(items=batch, cuda=True)
@@ -102,18 +104,18 @@ class EMATrainer:
                 loss_teacher = self.supervised_criterion(t_heatmap_pred, batch_heatmap)
                 
                 loss_meter_student.update(val=loss_student.item(),
-                                          weight=self.config.test_batch_size)
+                                          weight=num_item)
                 loss_meter_teacher.update(val=loss_teacher.item(),
-                                          weight=self.config.test_batch_size)
+                                          weight=num_item)
                 
                 # nme for model
                 nme_student = self.evaluator(s_keypoints_pred, batch)
                 nme_teacher = self.evaluator(t_keypoints_pred, batch)
                 
                 nme_meter_student.update(val=nme_student.item(),
-                                         weight=self.config.test_batch_size)
+                                         weight=num_item)
                 nme_meter_teacher.update(val=nme_teacher.item(),
-                                         weight=self.config.test_batch_size)
+                                         weight=num_item)
 
                 pbar.set_postfix({
                     'supervised loss student/teacher': [round(loss_meter_student.average(), 5),